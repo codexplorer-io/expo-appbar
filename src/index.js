7d00e2278961a70aae0757c601c10bbe184b56f6
Marko Matic <mail.marko.matic@gmail.com>
2022-04-28 10:29:00 +1000

    - Initial implementation

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+    Appbar as DefaultAppbar,
+    useTheme
+} from 'react-native-paper';
+import { Loading } from './styled';
+
+let isStatusBarTranslucent = true;
+export const setIsStatusBarTranslucent = isTranslucent => {
+    isStatusBarTranslucent = isTranslucent;
+};
+
+export const Appbar = ({ ...props }) => (
+    <DefaultAppbar.Header
+        {...(isStatusBarTranslucent ? {} : { statusBarHeight: 0 })}
+        {...props}
+    />
+);
+
+export const AppbarContent = ({ ...props }) => {
+    const { colors: { onPrimary } } = useTheme();
+
+    return (
+        <DefaultAppbar.Content
+            color={onPrimary}
+            {...props}
+        />
+    );
+};
+
+export const AppbarAction = ({ ...props }) => {
+    const { colors: { onPrimary } } = useTheme();
+
+    return (
+        <DefaultAppbar.Action
+            color={onPrimary}
+            {...props}
+        />
+    );
+};
+
+export const AppbarBackAction = ({ ...props }) => {
+    const { colors: { onPrimary } } = useTheme();
+
+    return (
+        <DefaultAppbar.BackAction
+            color={onPrimary}
+            {...props}
+        />
+    );
+};
+
+export const AppbarLoading = () => {
+    const { colors: { onPrimary } } = useTheme();
+
+    return (
+        <Loading
+            animating
+            color={onPrimary}
+        />
+    );
+};
---
+++ src/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+import { ActivityIndicator } from 'react-native-paper';
+
+export const Loading = styled(ActivityIndicator)`
+    margin-left: 12px;
+    margin-right: 12px;
+`;
